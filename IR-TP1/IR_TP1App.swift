c9d95232e67d0c6d4a27889d726add929537eeb6
Max Prudhomme <account@maxprudhomme.com>
2025-09-16 10:16:33 +0200

    Initial Commit

---
+++ IR-TP1/IR_TP1App.swift
@@ -0,0 +1,17 @@
+//
+//  IR_TP1App.swift
+//  IR-TP1
+//
+//  Created by Max PRUDHOMME on 16/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct IR_TP1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
